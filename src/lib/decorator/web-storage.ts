97b3d219009ec0947251de441d791438a2af2969
iwe7 <admin@meepo.com.cn>
2018-06-06 08:23:37 +0800

    update

--- src/lib/decorator/web-storage.ts
+++ src/lib/decorator/web-storage.ts
@@ -1,10 +1,5 @@
 import { WebStorageUtility } from "../utility/web-storage";
-export function LocalStorage(key?: string) {
-  return WebStorage(localStorage, key);
-}
-export function SessionStorage(key?: string) {
-  return WebStorage(sessionStorage, key);
-}
+
 const cache = {};
 export let WebStorage = (webStorage: Storage, key: string) => {
   return (target: Object, propertyName: string): void => {
@@ -12,12 +7,19 @@ export let WebStorage = (webStorage: Storage, key: string) => {
     const storageKey = WebStorageUtility.generateStorageKey(key);
     const storedValue = WebStorageUtility.get(webStorage, key);
     Object.defineProperty(target, propertyName, {
-      get: function() {
+      get: function () {
         return WebStorageUtility.get(webStorage, key);
       },
-      set: function(value: any) {
+      set: function (value: any) {
         WebStorageUtility.set(webStorage, key, value);
       }
     });
   };
 };
+
+export function LocalStorage(key?: string) {
+  return WebStorage(localStorage, key);
+}
+export function SessionStorage(key?: string) {
+  return WebStorage(sessionStorage, key);
+}
